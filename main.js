91444f55d22a1dd4ebbb51008b3352cd334e3dd4
xusniya123 <xusniya12345678@gmail.com>
2024-04-01 16:53:29 +0500

    Lesson 4 moth (11)

---
+++ main.js
@@ -0,0 +1,109 @@
+                                        // 1-masala
+// function min(a,b) {
+//     let c;
+//     c = Math.min(a,b);
+//     alert("kiritilgan 2 ta sonning kichigi = " + c);
+// }
+
+// let birinchi = Number(prompt("1 chi soni kiriting..."));
+// let ikkinchi = Number(prompt("2 chi soni kiriting..."));
+// min(birinchi,ikkinchi);
+
+
+                                                    //   2-masala
+
+// function min(a,b,d) {
+//     let c;
+//     c = Math.min(a,b,d);
+//     alert("kiritilgan 3 ta sonning kichigi = " + c);
+// }
+
+// let birinchi = Number(prompt("1 chi soni kiriting..."));
+// let ikkinchi = Number(prompt("2 chi soni kiriting..."));
+// let uchinchi = Number(prompt("3 chi soni kiriting..."));
+
+
+// min(birinchi,ikkinchi, uchinchi);
+
+                                                    //   3-masala
+// function lenghtArray(param1) {
+//     let l;
+//     l = param1.lenght;
+//     alert(l)
+// }
+
+// lenghtArray("Ali","Vali");
+                                                    //  4-masala
+// function plus() {
+//     const array =[];
+//  let birinchi = prompt("1 chi soni kiriting...");
+//  let ikkinchi = prompt("2 chi soni kiriting...");
+//  let uchinchi = prompt("3 chi soni kiriting...");
+// let r = array.push(birinchi,ikkinchi,uchinchi);
+// console.log(array);
+// }
+
+
+// plus();
+                                      // UY ISHI
+// 1) Ikkita o'zgaruvchi yarating va ularning ichiga son saqlang va bir birini cosole.logda qo'shib chiqaring chiqaring
+
+// let sport = 5;
+// let sport2 = 4;
+// console.log(sport+sport2);
+
+
+
+// 2) let a  = '12' shu 12 sonini data typeni o'zgartirib number qilib chiqaring 
+// bering. Yani bu hozir string typeda siz number tyga o'zgartiring
+
+// let a = +'12';
+// console.log(a);
+
+
+
+// 3) randomli son yasang va shu randomli sonning juft yoki toq ekanligini aniqlab bering
+
+// const randomNumber = Math.floor(Math.random() * 100) + 1;
+
+// if (randomNumber % 2 === 0) {
+//     alert(randomNumber + " soni juft");
+// } else {
+//     alert(randomNumber + " soni toq");
+// }
+
+
+// 4) promptdan bitta so'z kiriting agar o'sha so'z Mars so'ziga teng bolsa o'sha so'zning
+//  o'zi chiqsin agar boshqa so'z bo'lsa Mars It school deb chiqsin
+
+// let word = prompt("");
+
+// if (word === "Mars") {
+//     alert(word)
+// } else {
+//     alert("Mars It school")
+// }
+
+
+// 5) bitta array yarating va uning ichiga 5 ta so'z kiriting. Keyin For yordamida o'sha arraydagi 
+// so'zlar ichidan uzunligi 5 va undan kattalarini bohsqa bir arrayga qo'shib chiqaring
+
+
+
+
+// 6) for orqali birdan 100gacha bolgan sonlarni juft yoki toqqa ajratib chiqaring
+
+// function checkEvenOrOdd(number) {
+//     if (number % 2 === 0) {
+//         return "juft";
+//     } else {
+//         return "toq";
+//     }
+// }
+
+// for (let i = 1; i <= 100; i++) {
+//     console.log(i + " " + checkEvenOrOdd(i));
+// }
+
+ 
+
